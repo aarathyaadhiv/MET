7f4931aa1901d347da45d1acad63b1e95a9b3892
aarathyaadhiv <aarathyaadhiv@gmail.com>
2024-03-05 21:38:10 +0530

    chat model changed

--- pkg/utils/models/chat.go
+++ pkg/utils/models/chat.go
@@ -7,13 +7,12 @@ import (
 )
 
 type Chat struct {
-	ID        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Users     []uint             `json:"users" bson:"users"`
-	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
+	ID              primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Users           []uint             `json:"users" bson:"users"`
+	LastMessage     string             `json:"last_message" bson:"last_message"`
+	LastMessageTime time.Time          `json:"last_message_time" bson:"last_message_time"`
 }
 
 type Message struct {
 	Message string `json:"message"`
 }
-
-
